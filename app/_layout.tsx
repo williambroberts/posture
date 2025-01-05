ea3a5fb0da8627f838cb331ab16b99d2e63deecd
= <3williamroberts@gmail.com>
2025-01-05 12:56:26 +0000

    added permissiosn

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from "expo-router";
-
+//https://egghead.io/lessons/react-native-create-a-development-build-for-android-with-eas
 export default function RootLayout() {
   return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { DeviceMotion, DeviceMotionMeasurement } from 'expo-sensors';
+
+import { useEffect, useState } from "react";
 
 export default function Index() {
+  const [isGranted,setIsGranded] = useState(false)
+  const [data,setData] = useState<DeviceMotionMeasurement|null>(null)
+  useEffect(()=>{
+    (async () => {
+      const permissionResponse = await DeviceMotion.requestPermissionsAsync();
+      if (permissionResponse.granted){
+        setIsGranded(permissionResponse.granted)
+        return;
+      }
+      if (permissionResponse.granted === false && permissionResponse.canAskAgain){
+        const response = await DeviceMotion.requestPermissionsAsync();
+        setIsGranded(response.granted)
+        return;
+      }
+    })()
+  },[])
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>{JSON.stringify(data,null,2)}.</Text>
     </View>
-  );
+  )
 }
