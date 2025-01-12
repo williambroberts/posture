08a4db2fc3670b1b5eedf65e1b736054fc65cbba
= <3williamroberts@gmail.com>
2025-01-12 13:04:09 +0000

    add sub to page

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, View } from 'react-native'
+import { gyroscope, SensorData } from "react-native-sensors";
 
 export default function Index(){
-
+  const [data,setData] = useState<SensorData>()
+  
+  useEffect(()=>{
+    const subscription = gyroscope.subscribe((data) =>
+      setData(data)
+    );
+    return () =>{
+      subscription.remove(()=>{
+        console.log("unsubsribed")
+      });
+    }
+    
+  },[])
   return (
   <View>
-    <Text>hi</Text>
+    <Text>{JSON.stringify(data,null,2)}</Text>
   </View>
   )
 }
